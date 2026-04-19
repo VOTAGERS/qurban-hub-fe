83e8f3473cb59058e6d3b6a54f900a2e78ed1d6c
imurra <imamnc.developer@gmail.com>
2026-04-19 12:56:12 +0700

    - add form via woocomerce

--- functions.php
+++ functions.php
@@ -84,3 +84,100 @@ function sports_outlet_enqueue_admin_script() {
 	);
 }
 add_action( 'admin_enqueue_scripts', 'sports_outlet_enqueue_admin_script' );
+
+
+add_action('woocommerce_before_cart', 'custom_form_di_cart');
+
+function custom_form_di_cart() {
+    ?>
+    <div class="cart-custom-form">
+        <h3>Data User</h3>
+
+        <p>
+            <label>Full Name</label>
+            <input type="text" name="full_name" id="full_name">
+        </p>
+
+        <p>
+            <label>Email</label>
+            <input type="email" name="email" id="email">
+        </p>
+
+        <p>
+            <label>Phone Number</label>
+            <input type="text" name="phone_number" id="phone_number">
+        </p>
+    </div>
+    <?php
+}
+
+add_action('wp_footer', 'cart_form_script');
+
+function cart_form_script() {
+    if (!is_cart()) return;
+?>
+	<script>
+	document.querySelectorAll('#full_name, #email, #phone_number').forEach(el => {
+		el.addEventListener('change', function() {
+
+			fetch('<?php echo admin_url('admin-ajax.php'); ?>', {
+				method: 'POST',
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+				body: new URLSearchParams({
+					action: 'save_cart_form',
+					full_name: document.getElementById('full_name').value,
+					email: document.getElementById('email').value,
+					phone_number: document.getElementById('phone_number').value
+				})
+			});
+
+		});
+	});
+	</script>
+<?php
+}
+
+add_action('wp_ajax_save_cart_form', 'save_cart_form');
+add_action('wp_ajax_nopriv_save_cart_form', 'save_cart_form');
+
+function save_cart_form() {
+    WC()->session->set('custom_full_name', $_POST['full_name']);
+    WC()->session->set('custom_email', $_POST['email']);
+    WC()->session->set('custom_phone', $_POST['phone_number']);
+    wp_die();
+}
+
+
+
+add_action('woocommerce_checkout_fields', 'prefill_checkout');
+
+
+function prefill_checkout($fields) {
+
+    $fields['billing']['billing_first_name']['default'] = WC()->session->get('custom_full_name');
+    $fields['billing']['billing_email']['default'] = WC()->session->get('custom_email');
+    $fields['billing']['billing_phone']['default'] = WC()->session->get('custom_phone');
+
+    return $fields;
+}
+
+
+add_action('woocommerce_checkout_order_processed', 'kirim_ke_laravel', 10, 3);
+
+function kirim_ke_laravel($order_id, $posted_data, $order) {
+
+    $data = [
+        'full_name' => WC()->session->get('custom_full_name'),
+        'email' => WC()->session->get('custom_email'),
+        'phone_number' => WC()->session->get('custom_phone'),
+        'status' => 'active',
+        'created_by' => 'woocommerce',
+        'updated_by' => 'woocommerce',
+    ];
+
+    wp_remote_post('https://domain-laravel.com/api/users', [
+        'body' => $data
+    ]);
+}
+
+
